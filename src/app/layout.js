ce963c08a2e3aa77c77ee243dbb8a5551fb0640d
Andy <andy.lamyt@gmail.com>
2025-09-16 11:40:59 +0100

    added background, imported from react bits

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+import LiquidEther from "@/components/LiquidEther";
 
 library.add(fas, far, fab);
 
@@ -25,7 +26,28 @@ export default function RootLayout({ children }) {
     <html lang="en">
       {/* #ea590b  #1d1d1d #4b4b4b/}
       {/* xl:px-35 md:px-10 px-4 */}
-      <body className={`bg-[#1d1d1d] ${poppins.className}`}>{children}</body>
+      <body className={`bg-[#1d1d1d] text-[#3dbcf7] ${poppins.className}`}>
+        <div className="fixed w-full h-full inset-0 ">
+          <LiquidEther
+            colors={["#5227FF", "#3dbcf7", "#B19EEF"]}
+            mouseForce={20}
+            cursorSize={100}
+            isViscous={false}
+            viscous={30}
+            iterationsViscous={32}
+            iterationsPoisson={32}
+            resolution={0.5}
+            isBounce={false}
+            autoDemo={true}
+            autoSpeed={0.5}
+            autoIntensity={2.2}
+            takeoverDuration={0.25}
+            autoResumeDelay={3000}
+            autoRampDuration={0.6}
+          />
+        </div>
+        <div className="absolute inset-0">{children}</div>
+      </body>
     </html>
   );
 }
