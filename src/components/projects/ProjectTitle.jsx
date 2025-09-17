7a3e200710440f485ddf8e58a195e29e929b950f
Andy <andy.lamyt@gmail.com>
2025-09-17 12:26:29 +0100

    fixed: project title is now centered on small device

--- src/components/projects/ProjectTitle.jsx
+++ src/components/projects/ProjectTitle.jsx
@@ -2,7 +2,7 @@ function ProjectTitle({ data }) {
   // console.log(data);
   return (
     <div className="flex justify-center lg:mt-16 text-4xl">
-      <div className=" text-[#474fc9] font-medium tracking-wide gap-6">
+      <div className=" text-[#474fc9] font-medium tracking-wide gap-6 text-center">
         <h3>{data.title.toUpperCase()}</h3>
         <div className="py-3">
           <p className="text-base align-bottom text-[#9a9bbf] text-center">
